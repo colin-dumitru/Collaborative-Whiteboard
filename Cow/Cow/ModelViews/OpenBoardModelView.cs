0b83e988bd6e528fd9eeee76212da00797179e34
Catalin Dumitru <catalin.dumitru@live.com>
2012-01-08 13:14:09 +0200

    Adaugat bara de de modificarea layer-urilor in view

---
+++ Cow/Cow/ModelViews/OpenBoardModelView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cow.ModelViews {
+    public class OpenBoardModelView {
+        /*id-ul tablei*/
+        public int BoardId { get; set; }
+        /*numele tablei*/
+        public String Name { get; set; }
+    }
+}
